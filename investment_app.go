0e1ecca98a841357f34ed003dde7e777bab501b5
yray akemu <yrayak@outlook.com>
2024-09-10 23:43:16 -0400

    added users input for dynamic variable value

--- investment_app.go
+++ investment_app.go
@@ -6,11 +6,27 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	const inflationRate float64 = 2.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
 
-	var futureValue = float64(investmentAmount) * math.Pow(1 + expectedReturnRate / 100, float64(years))
+	fmt.Print("Investment amount: ")
+	// scan the terminal for user input
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	
+
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+
+	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }

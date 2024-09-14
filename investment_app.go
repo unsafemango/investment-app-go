e2949b18adc09833625c83e116d3693ca02bb3f7
yray akemu <yrayak@outlook.com>
2024-09-13 23:58:46 -0400

    created futurevalues functionality to return the calculated future values

--- investment_app.go
+++ investment_app.go
@@ -5,14 +5,15 @@ import (
 	"math"
 )
 
+const inflationRate float64 = 2.5
+
 func main() {
-	const inflationRate float64 = 2.5
 	var investmentAmount float64
 	var years float64
 	var expectedReturnRate float64
 
-	fmt.Print("Investment amount: ")
 	// scan the terminal for user input
+	fmt.Print("Investment amount: ")
 	fmt.Scan(&investmentAmount)
 
 	fmt.Print("Expected return rate: ")
@@ -21,12 +22,32 @@ func main() {
 	fmt.Print("Years: ")
 	fmt.Scan(&years)
 
-	
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+	// futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	// futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
 
-	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	// save formatted data and text in variables
+	formattedFV := fmt.Sprintf("Future value: %.2f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future value (adjusted for inflation): %.2f\n", futureRealValue)
 
-	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
+	// output information
+	// fmt.Println("Future value:", futureValue)
+	// fmt.Printf("Future value: %.2f\n", futureValue)
+	// fmt.Printf("Future value (adjusted for inflation): %.2f", futureRealValue)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	// fmt.Print(formattedFV, formattedRFV)
+	outputText(formattedFV, formattedRFV)
+}
+
+func outputText(text, text2 string){
+	fmt.Print(text, text2)
+}
+
+// function that returns 2 float64 values
+func calculateFutureValues(investmentAmount float64, expectedReturnRate float64, years float64) (fv float64, rfv float64) {
+	fv = investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	rfv = fv / math.Pow(1 + inflationRate/100, years)
+
+	return fv, rfv
+	// return
 }
